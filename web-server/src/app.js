580cb34a26082f36486f73508cfe9c4feeab705e
Deep <deepsingh.cts@gmail.com>
2019-10-09 01:10:57 +0530

    configuring handlebars for express-hbs

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -5,7 +5,12 @@ const app = express();
 const port = 3000;
 const publicPath = path.join(__dirname, '../public');
 
+app.set('view engine', 'hbs');
 app.use(express.static(publicPath));
+
+app.get('/', (req, res)=>{
+    res.render('index')
+})
 
 app.listen(port, () => {
     console.log('Server is up on port ', port)
