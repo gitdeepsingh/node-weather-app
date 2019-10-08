2aa42c6ff2a02d122516139f4a25a69f83085a5d
Deep <deepsingh.cts@gmail.com>
2019-10-08 15:43:35 +0530

    weather app setup

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,9 +1,37 @@
 const request = require('request');
-const token='765ddcafd93200cabfecb72c20a7fcd1'
-const url = `https://api.darksky.net/forecast/${token}/12.9716,77.5946?units=si`;
+const chalk = require('chalk');
 
-request({ url: url, json: true }, (err, res) => {
-    const data=res.body.currently;
-    console.log(data);
+const wToken = '765ddcafd93200cabfecb72c20a7fcd1';
+const gToken = 'pk.eyJ1IjoiZGVlcHNpbmdoIiwiYSI6ImNrMWhrdjZqbzAzYm0zY2p6andwdHJiNG0ifQ.oFF0K-7GKc7VAYiJiECfSA';
+
+const weatherUrl = `https://api.darksky.net/forecast/${wToken}/12.9716,77.5946?units=si`;
+const geoUrl = `https://api.mapbox.com/geocoding/v5/mapbox.places/Bangalore.json?access_token=${gToken}&limt=1`;
+
+request({ url: weatherUrl, json: true }, (err, res) => {
+    if (err) {
+        console.log(chalk.red('Unable to connect to the weather service. Please try later.'));
+
+    } else if (res.body.error) {
+        console.log(chalk.red('Location not found!'));
+
+    } else {
+        const data = res.body.currently.temperature;
+        console.log(chalk.blue('temperature: '), data);
+    }
+});
+
+request({ url: geoUrl, json: true }, (err, res) => {
+     if (err) {
+        console.log(chalk.red('Unable to connect to the geolocation service. Please try later.'));
+
+    } else if (res.body.features.length === 0) {
+        console.log(chalk.red('Unable to find this location! Try another'));
+
+    } else {
+    const long = res.body.features[0].center[0];
+    const lat = res.body.features[0].center[1];
+    console.log(chalk.green('longitude: '), long);
+    console.log(chalk.green('latitude: '), lat);
+    }
 });
 
