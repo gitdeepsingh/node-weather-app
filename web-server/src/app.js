602b237090f50b261ec0326a4551c9af4c90687c
Deep <deepsingh.cts@gmail.com>
2019-10-09 00:53:00 +0530

    establishing web server to render static content

---
+++ web-server/src/app.js
@@ -0,0 +1,12 @@
+const path = require('path')
+const express = require('express');
+
+const app = express();
+const port = 3000;
+const publicPath = path.join(__dirname, '../public');
+
+app.use(express.static(publicPath));
+
+app.listen(port, () => {
+    console.log('Server is up on port ', port)
+})
