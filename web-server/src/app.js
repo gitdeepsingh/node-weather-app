693395558bdfd18f15edd325cbcad4c7240e2bd9
Deep <deepsingh.cts@gmail.com>
2019-10-09 13:08:12 +0530

    added 404 page

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -10,7 +10,7 @@ const port = 3000;
 //path for express config
 const publicPath = path.join(__dirname, '../public');
 const viewPath = path.join(__dirname, '../templates/views');
-const partialsPath=path.join(__dirname, '../templates/partials')
+const partialsPath = path.join(__dirname, '../templates/partials')
 
 
 //set up handlebars engine and views location
@@ -21,24 +21,38 @@ hbs.registerPartials(partialsPath);
 //static directory to serve
 app.use(express.static(publicPath));
 
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
     res.render('index', {
         title: 'My Dashboard',
         description: 'I am a Software Developer'
     })
 })
 
-app.get('/about', (req, res)=>{
+app.get('/about', (req, res) => {
     res.render('about', {
         title: 'About Me',
         description: 'This is my porfolio.'
     })
 })
 
-app.get('/help', (req, res)=>{
+app.get('/help', (req, res) => {
     res.render('help', {
         title: 'Need Help?',
         description: 'Please contact me at deepsinghh.js@gmail.com'
+    })
+})
+
+app.get('/help/*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        description: 'Help article not found!'
+    })
+})
+
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: '404',
+        description: 'Page not found :('
     })
 })
 
